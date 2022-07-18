c45946b32a7b4ec0798828f4fff1702174fd1164
Willian <willian.alves.b15@gmail.com>
2022-07-18 11:35:44 -0300

    feat: primeira requisição java

--- alura-stickers-movie/src/App.java
+++ alura-stickers-movie/src/App.java
@@ -1,5 +1,18 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String url = "https://imdb-api.com/pt/API/Top250Movies/k_hami0zaa";
+        URI endPoint = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endPoint).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
     }
 }
