5830b01306bf624f4fbc70eecff79826e660e99b
Willian <willian.alves.b15@gmail.com>
2022-07-19 11:29:39 -0300

    feat: list movies

--- alura-stickers-movie/src/App.java
+++ alura-stickers-movie/src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -13,6 +15,15 @@ public class App {
         var request = HttpRequest.newBuilder(endPoint).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+
+        var parser = new JsonParse();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
