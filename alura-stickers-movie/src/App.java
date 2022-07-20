01d6a0f1f863bcbadfec11fa40a9d8e43fa88fcc
Willian <willian.alves.b15@gmail.com>
2022-07-20 10:53:37 -0300

    feat: adicionando metodo da nova class geradorFigurinhas no app

--- alura-stickers-movie/src/App.java
+++ alura-stickers-movie/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -23,6 +25,21 @@ public class App {
             System.out.println(filme.get("title"));
             System.out.println(filme.get("image"));
             System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+
+        var geradora = new GeradorFigurinhas();
+        for (Map<String,String> filme : listaDeFilmes) {
+
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.criar(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
             System.out.println();
         }
     }
