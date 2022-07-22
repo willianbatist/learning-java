2e0b303b2d3748b2aac41dba6651d677c4803ada
Willian <willian.alves.b15@gmail.com>
2022-07-22 17:20:27 -0300

    feat: class LinguagemController com metodo obterLinguagens

--- linguagens-api/src/main/java/br/com/learning/linguagensapi/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/learning/linguagensapi/LinguagemController.java
@@ -1,13 +1,26 @@
 package br.com.learning.linguagensapi;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LinguagemController {
 
-    @GetMapping(value="/linguagem")
+    private List<Linguagem> linguagens =
+      List.of(
+        new Linguagem("javaScript", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/javascript/javascript_256x256.png", 1),
+        new Linguagem("typeScript", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/typescript/typescript_256x256.png", 2)
+      );
+
+    @GetMapping("/hello")
     public String getMethodName() {
       return "hello Java, willian";
+    }
+
+    @GetMapping("/linguagens")
+    public List<Linguagem> obterLinguagens() {
+      return linguagens;
     }
 }
