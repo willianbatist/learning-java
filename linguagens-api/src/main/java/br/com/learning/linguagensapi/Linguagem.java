1d5013624bd5616cb5a0b4e6ec8f4c972e7d1e8f
Willian <willian.alves.b15@gmail.com>
2022-07-22 17:19:10 -0300

    feat: class Linguagem recebendo collection principaisLinguagens

--- linguagens-api/src/main/java/br/com/learning/linguagensapi/Linguagem.java
+++ linguagens-api/src/main/java/br/com/learning/linguagensapi/Linguagem.java
@@ -1,6 +1,12 @@
 package br.com.learning.linguagensapi;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "principaisLinguagens")
 public class Linguagem {
+    @Id
+    private String id;
     private String title;
     private String image;
     private int ranking;
@@ -10,6 +16,11 @@ public class Linguagem {
         this.image = image;
         this.ranking = ranking;
     }
+
+    public String getId() {
+        return id;
+    }
+
     public String getTitle() {
         return title;
     }
