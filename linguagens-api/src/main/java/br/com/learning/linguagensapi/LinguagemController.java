f7694438a3d9d77f8529d4c3d074ead13b78edff
Willian <willian.alves.b15@gmail.com>
2022-07-22 20:16:15 -0300

    fix: refatorando obterLinguagens

--- linguagens-api/src/main/java/br/com/learning/linguagensapi/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/learning/linguagensapi/LinguagemController.java
@@ -1,26 +1,32 @@
 package br.com.learning.linguagensapi;
 
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LinguagemController {
 
-    private List<Linguagem> linguagens =
-      List.of(
-        new Linguagem("javaScript", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/javascript/javascript_256x256.png", 1),
-        new Linguagem("typeScript", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/typescript/typescript_256x256.png", 2)
-      );
+    // private List<Linguagem> linguagens =
+    //   List.of(
+    //     new Linguagem("javaScript", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/javascript/javascript_256x256.png", 1),
+    //     new Linguagem("typeScript", "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/typescript/typescript_256x256.png", 2)
+    //   );
 
     @GetMapping("/hello")
     public String getMethodName() {
       return "hello Java, willian";
     }
 
+    @Autowired
+    private LinguagemRepository repository;
+
     @GetMapping("/linguagens")
     public List<Linguagem> obterLinguagens() {
+      List<Linguagem> linguagens = repository.findAll();
       return linguagens;
     }
 }
