544fbaa013d5ceeaaf198b5acc4ff304c9251860
Willian <willian.alves.b15@gmail.com>
2022-07-22 22:00:43 -0300

    feat: metodo delete

--- linguagens-api/src/main/java/br/com/learning/linguagensapi/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/learning/linguagensapi/LinguagemController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -30,5 +32,10 @@ public class LinguagemController {
     public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
       Linguagem linguagemSalva = repository.save(linguagem);
       return linguagemSalva;
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public void deletePost(@PathVariable("id") String id) {
+      repository.deleteById(id);
     }
 }
